f481e3662c63fb79d2af10e79ff5013339692a7d
ilisa <ReeCORDee@gmail.com>
2023-09-28 02:05:01 +0300

    n1

--- T01/Program.cs
+++ T01/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] arr = { 5, 7, 2, 5, 3, 8, 1, 6 };
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionArray(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int min = i;
+        for (int j = i + 1; j < array.Length; j++)
+            if (array[j] < array[min])
+            {
+                min = j;
+            }
+        int temp = array[min];
+        array[min] = array[i];
+        array[i] = temp;
+    }
+
+}
+
+PrintArray(arr);
+SelectionArray(arr);
+PrintArray(arr);
