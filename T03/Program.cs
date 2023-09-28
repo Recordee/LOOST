cf9e0bed5f953b8a50e94a2030d97939e2b2867d
ilisa <ReeCORDee@gmail.com>
2023-09-29 01:23:12 +0300

    n4

---
+++ T03/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая по
+// заданному номеру четверти, показывает диапазон
+// возможных координат точек в этой четверти (x и y).
+
+double number  = 3.09232321;
+double numberRound = Math.Round(number,2);
+Console.WriteLine(numberRound);
+
+double sqrt = Math.Sqrt(5);
