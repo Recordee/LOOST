40c409b27d8d5eb92866d842436dcfea48f98b81
ilisa <ReeCORDee@gmail.com>
2023-09-28 14:50:41 +0300

    rename

---
+++ Sort/Program.cs
@@ -0,0 +1,31 @@
+﻿int[] arr = { 5, 7, 2, 5, 3, 8, 1, 6 };
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionArray(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int min = i;
+        for (int j = i + 1; j < array.Length; j++)
+            if (array[j] < array[min])
+            {
+                min = j;
+            }
+        int temp = array[min];
+        array[min] = array[i];
+        array[i] = temp;
+    }
+
+}
+
+PrintArray(arr);
+SelectionArray(arr);
+PrintArray(arr);
