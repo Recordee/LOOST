fbc5c670d22ba889175a0b96b321347e1f43c27c
ilisa <ReeCORDee@gmail.com>
2023-09-29 23:45:03 +0300

    0

--- Цикл/Program.cs
+++ Цикл/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void PrintMatrix(int[,] matr)
+{
+    for(int row = 0;row < matr.GetLength(0);row++)
+        {
+        for(int colomns = 0;colomns < matr.GetLength(1);colomns++)
+            {
+                Console.Write($"{matr[row,colomns]} ");
+            }
+        Console.WriteLine();
+        }       
+}
+
+int[,] RandomNumber(int[,] matrRandom) // void
+{
+    for(int row = 0;row < matrRandom.GetLength(0);row++)
+        {
+        for(int colomns = 0;colomns < matrRandom.GetLength(1);colomns++)
+            {
+                int number = new Random().Next(1,100);
+                matrRandom[row,colomns] = number;
+            }
+        }
+    return matrRandom;
+}
+
+int[,] matrix = new int[3,4];
+PrintMatrix(matrix);
+RandomNumber(matrix);
+Console.WriteLine();
+PrintMatrix(matrix);
