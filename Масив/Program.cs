51d11c5f6fabb44d98efceea70dceba18b4f17e2
ilisa <ReeCORDee@gmail.com>
2023-10-04 00:14:49 +0300

    04.10

---
+++ Масив/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] arr = new int[12];
+
+void FillArray( int[] arr)
+{
+    Random rd = new Random();
+    for(int i =0;i < arr.Length;i++)
+    {
+        arr[i] = rd.Next(-9,9);
+    }
+}
+
+void PrintArray( int[] arr)
+{
+    for(int i =0;i < arr.Length;i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+FillArray(arr);
+PrintArray(arr);
--- Оптимальный код/Program.cs
+++ Оптимальный код/Program.cs
@@ -1,3 +1,46 @@
-﻿string path = "C:/Users/ilisa/OneDrive/ЗАНЯТИЯ/Инфа/ASD";
-DirectoryInfo di = new DirectoryInfo(path);
-System.Console.WriteLine(di.CreationTime);
+﻿decimal kRec=0;
+decimal kIter=0;
+int number = Convert.ToInt32(Console.ReadLine());
+
+// decimal FiboRec(int n)
+// {
+//     kRec++;
+//     if (n==1 || n==2) return 1;
+//     return FiboRec(n-1)+FiboRec(n-2);   
+// }
+
+
+decimal FiboIter(int n)
+{
+    int res=1;
+    int f0 = 1;
+    int f1 = 1;
+    kIter++;
+    for(int i = 2; i<n;i++)
+    {       
+        kIter++;
+        res = f0+f1;
+        f0=f1;
+        f1=res;
+    }
+    return res;
+}
+
+
+// for(int i =1; i <number;i++)
+// {
+//     Console.WriteLine($"Число фибоначи № {i} = {FiboRec(i)} понадобилось сделать {kRec} запусков");
+//     kRec=0;
+// }
+
+// Console.WriteLine("-----------------");
+
+for(int n =1; n <number;n++)
+{
+    Console.WriteLine($"Число фибоначи № {n} = {FiboIter(n)} понадобилось сделать {kIter} запусков");
+    kIter=0;
+}
+
+
+// FiboRec(number);
+// FiboIter(number);
